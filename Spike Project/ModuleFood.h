c66da7b855736e2fa30cb668928928e5718747a5
Jony635 <jony635molina@gmail.com>
2017-09-08 13:59:11 +0200

    Class Food Created

--- Spike Project/ModuleFood.h
+++ Spike Project/ModuleFood.h
@@ -1,7 +1,41 @@
 #ifndef MODULEFOOD
 #define MODULEFOOD
-class Food {
 
+#include "Globals.h"
+#include "Animation.h"
+#include "Module.h"
+
+enum FoodTypes 
+{
+	NO_TYPE,
+	PINEAPPLE,
+	ONION,
+	POOL_BALL,
+	SAUSAGE,
+	EGG,
+	CHICKEN,
+	COOKIE,
+	NACHO,
+	CANDY
+};
+
+class Food {
+public:
+	FoodTypes Type = FoodTypes::NO_TYPE;
+	uint speed = 0;
+	Animation Anim;
+
+	Food(FoodTypes type, uint speed) : Type(type), speed(speed) {}
+	~Food(){}
+};
+
+class ModuleFood :: public Module{
+public:
+	ModuleFood(){}
+	~ModuleFood(){}
+	Update(){}
+	Clean(){}
+	Start(){}
 };
 
 
